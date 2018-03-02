b9f70f8fa83e423efc47fd8d287fa450b785a03f
Arnabjyoti Kalita <arnabjyotikalita35@gmail.com>
2018-03-02 05:22:16 -0500

    Sleep no longer uses int

--- bin/sleep/sleep.c
+++ bin/sleep/sleep.c
@@ -7,14 +7,14 @@
 int main(int argc, char **argv, char **envp)
 {
   int seconds;
-  int err;
+  //int err;
 
   if(argc <= 1)
     exit(1);
 
   seconds = atoi(argv[1]);
 
-  err = sleep(seconds);  // different syscall ?
+  sleep(seconds);  // different syscall ?
   //printf("task waking up from sleep with error is %d\n", err);
-  return (err < 0) ? EXIT_FAILURE : EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
